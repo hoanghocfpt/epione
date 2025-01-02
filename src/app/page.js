b6ae48c9fc62affb1278a334734985bb39aee543
Phạm Văn Hoàng <iloveanh18@gmail.com>
2025-01-02 16:36:16 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,163 @@
-import Image from "next/image";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <h1>welcome to home</h1>
+      <div className='max-w-screen-2xl relative px-8 mx-auto'>
+        <div className='blur-lg absolute top-0 left-0 -z-10 w-full h-full'>
+          <Image width={700} height={700} className='w-full' src='/banner-xmas.webp' alt='banner' />
+        </div>
+        <Image width={1000} height={1000} className='w-full translate-y-10 mb-10 rounded-3xl' src='/banner-xmas.webp' alt='banner' />
+      </div>
+      <div className='max-w-screen-2xl mx-auto px-8 py-10'>
+        <h2 className='text-[44px] font-bold text-center mb-6'>Danh mục sản phẩm</h2>
+        <div className='grid grid-cols-3 gap-8'>
+          <Link href='/categories/ghe-cong-thai-hoc'>
+            <Image width={300} height={300} className='w-full mb-2' src='/collection-ghe-cong-thai-hoc.webp' alt='chair' />
+            <h3 className='text-[20px] text-[#10069f] text-center font-bold'>Ghế công thái học</h3>
+            <span className='block text-center text-[#929292] font-medium'>5 sản phẩm</span>
+          </Link>
+          <Link href='/categories/ghe-cong-thai-hoc'>
+            <Image width={300} height={300} className='w-full mb-2' src='/collection-ghe-cong-thai-hoc.webp' alt='chair' />
+            <h3 className='text-[20px] text-[#10069f] text-center font-bold'>Ghế công thái học</h3>
+            <span className='block text-center text-[#929292] font-medium'>5 sản phẩm</span>
+          </Link>
+          <Link href='/categories/ghe-cong-thai-hoc'>
+            <Image width={300} height={300} className='w-full mb-2' src='/collection-ghe-cong-thai-hoc.webp' alt='chair' />
+            <h3 className='text-[20px] text-[#10069f] text-center font-bold'>Ghế công thái học</h3>
+            <span className='block text-center text-[#929292] font-medium'>5 sản phẩm</span>
+          </Link>
+        </div>
+      </div>
+      <div className='max-w-screen-2xl mx-auto px-8 py-10'>
+        <h2 className='text-[44px] font-bold text-center mb-6'>Sắm combo - Nhận ưu đãi</h2>
+        <div className='grid grid-cols-4 gap-7 mb-7'>
+          <Link href='/categories/ghe-cong-thai-hoc' className='group rounded-3xl overflow-hidden border-[1px] border-[#e5e5e5] shadow-xl shadow-[#10069f1a]'>
+            <div className='transition-transform aspect-[4/3] relative overflow-hidden'>
+              <Image width={500} height={500} className='absolute top-0 transition-all duration-500 group-hover:scale-105 opacity-0 group-hover:opacity-100 w-full mb-2 h-full object-cover' src='/pic10.jpg' alt='chair' />
+              <Image width={500} height={500} className='absolute top-0 duration-500 group-hover:scale-100 opacity-100 group-hover:opacity-0 w-full mb-2 h-full object-cover' src='/pic5.jpg' alt='chair' />
+            </div>
+            <div className='p-4 pt-0'>
+              <div className='bg-[#10069f] rounded-full py-[4px] px-[14px] w-fit mx-auto -translate-y-1/2'>
+                <span className='text-white font-medium text-[18px]'>COMBO</span>
+              </div>
+              <h3 className='text-[16px] group-hover:underline text-black text-center font-bold'>Setup Mark</h3>
+              <div className='flex flex-col items-center'>
+                <span className='text-[#10069f] font-medium mb-2 text-[26px]'>1.500.000 VND</span>
+                <span className='text-[#929292] font-medium text-[14px] mb-1 line-through'>2.000.000 VND</span>
+                <span className='bg-[#f83a3a] py-[2px] px-[10px] mb-3 font-medium text-white text-[14px] rounded-full'>-10%</span>
+              </div>
+              <div className='border-dashed rounded-2xl border border-[#d2d2d2] py-3 px-2'>
+                <span className='text-sm text-center mb-2 block'>Sản phẩm bao gồm</span>
+                <div className='flex items-center justify-center flex-wrap gap-1'>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Bàn</span>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Ghế công thái học</span>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Arm</span>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Kê chân</span>
+                </div>
+              </div>
+              <Link href='/categories/ghe-cong-thai-hoc' className='border-[1.5px] w-fit mx-auto hover:bg-[#10069f] hover:text-white border-[#10069f] rounded-full py-2 px-4 mt-4 block text-[#10069f] text-center font-medium'>
+                <div className='group-hover:underline'>Chi tiết COMBO</div>
+              </Link>
+            </div>
+          </Link>
+          <Link href='/categories/ghe-cong-thai-hoc' className='group rounded-3xl overflow-hidden border-[1px] border-[#e5e5e5] shadow-xl shadow-[#10069f1a]'>
+            <div className='transition-transform aspect-[4/3] relative overflow-hidden'>
+              <Image width={500} height={500} className='absolute top-0 transition-all duration-500 group-hover:scale-105 opacity-0 group-hover:opacity-100 w-full mb-2 h-full object-cover' src='/pic10.jpg' alt='chair' />
+              <Image width={500} height={500} className='absolute top-0 duration-500 group-hover:scale-100 opacity-100 group-hover:opacity-0 w-full mb-2 h-full object-cover' src='/pic5.jpg' alt='chair' />
+            </div>
+            <div className='p-4 pt-0'>
+              <div className='bg-[#10069f] rounded-full py-[4px] px-[14px] w-fit mx-auto -translate-y-1/2'>
+                <span className='text-white font-medium text-[18px]'>COMBO</span>
+              </div>
+              <h3 className='text-[16px] group-hover:underline text-black text-center font-bold'>Setup Mark</h3>
+              <div className='flex flex-col items-center'>
+                <span className='text-[#10069f] font-medium mb-2 text-[26px]'>1.500.000 VND</span>
+                <span className='text-[#929292] font-medium text-[14px] mb-1 line-through'>2.000.000 VND</span>
+                <span className='bg-[#f83a3a] py-[2px] px-[10px] mb-3 font-medium text-white text-[14px] rounded-full'>-10%</span>
+              </div>
+              <div className='border-dashed rounded-2xl border border-[#d2d2d2] py-3 px-2'>
+                <span className='text-sm text-center mb-2 block'>Sản phẩm bao gồm</span>
+                <div className='flex items-center justify-center flex-wrap gap-1'>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Bàn</span>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Ghế công thái học</span>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Arm</span>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Kê chân</span>
+                </div>
+              </div>
+              <Link href='/categories/ghe-cong-thai-hoc' className='border-[1.5px] w-fit mx-auto hover:bg-[#10069f] hover:text-white border-[#10069f] rounded-full py-2 px-4 mt-4 block text-[#10069f] text-center font-medium'>
+                <div className='group-hover:underline'>Chi tiết COMBO</div>
+              </Link>
+            </div>
+          </Link>
+          <Link href='/categories/ghe-cong-thai-hoc' className='group rounded-3xl overflow-hidden border-[1px] border-[#e5e5e5] shadow-xl shadow-[#10069f1a]'>
+            <div className='transition-transform aspect-[4/3] relative overflow-hidden'>
+              <Image width={500} height={500} className='absolute top-0 transition-all duration-500 group-hover:scale-105 opacity-0 group-hover:opacity-100 w-full mb-2 h-full object-cover' src='/pic10.jpg' alt='chair' />
+              <Image width={500} height={500} className='absolute top-0 duration-500 group-hover:scale-100 opacity-100 group-hover:opacity-0 w-full mb-2 h-full object-cover' src='/pic5.jpg' alt='chair' />
+            </div>
+            <div className='p-4 pt-0'>
+              <div className='bg-[#10069f] rounded-full py-[4px] px-[14px] w-fit mx-auto -translate-y-1/2'>
+                <span className='text-white font-medium text-[18px]'>COMBO</span>
+              </div>
+              <h3 className='text-[16px] group-hover:underline text-black text-center font-bold'>Setup Mark</h3>
+              <div className='flex flex-col items-center'>
+                <span className='text-[#10069f] font-medium mb-2 text-[26px]'>1.500.000 VND</span>
+                <span className='text-[#929292] font-medium text-[14px] mb-1 line-through'>2.000.000 VND</span>
+                <span className='bg-[#f83a3a] py-[2px] px-[10px] mb-3 font-medium text-white text-[14px] rounded-full'>-10%</span>
+              </div>
+              <div className='border-dashed rounded-2xl border border-[#d2d2d2] py-3 px-2'>
+                <span className='text-sm text-center mb-2 block'>Sản phẩm bao gồm</span>
+                <div className='flex items-center justify-center flex-wrap gap-1'>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Bàn</span>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Ghế công thái học</span>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Arm</span>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Kê chân</span>
+                </div>
+              </div>
+              <Link href='/categories/ghe-cong-thai-hoc' className='border-[1.5px] w-fit mx-auto hover:bg-[#10069f] hover:text-white border-[#10069f] rounded-full py-2 px-4 mt-4 block text-[#10069f] text-center font-medium'>
+                <div className='group-hover:underline'>Chi tiết COMBO</div>
+              </Link>
+            </div>
+          </Link>
+          <Link href='/categories/ghe-cong-thai-hoc' className='group rounded-3xl overflow-hidden border-[1px] border-[#e5e5e5] shadow-xl shadow-[#10069f1a]'>
+            <div className='transition-transform aspect-[4/3] relative overflow-hidden'>
+              <Image width={500} height={500} className='absolute top-0 transition-all duration-500 group-hover:scale-105 opacity-0 group-hover:opacity-100 w-full mb-2 h-full object-cover' src='/pic10.jpg' alt='chair' />
+              <Image width={500} height={500} className='absolute top-0 duration-500 group-hover:scale-100 opacity-100 group-hover:opacity-0 w-full mb-2 h-full object-cover' src='/pic5.jpg' alt='chair' />
+            </div>
+            <div className='p-4 pt-0'>
+              <div className='bg-[#10069f] rounded-full py-[4px] px-[14px] w-fit mx-auto -translate-y-1/2'>
+                <span className='text-white font-medium text-[18px]'>COMBO</span>
+              </div>
+              <h3 className='text-[16px] group-hover:underline text-black text-center font-bold'>Setup Mark</h3>
+              <div className='flex flex-col items-center'>
+                <span className='text-[#10069f] font-medium mb-2 text-[26px]'>1.500.000 VND</span>
+                <span className='text-[#929292] font-medium text-[14px] mb-1 line-through'>2.000.000 VND</span>
+                <span className='bg-[#f83a3a] py-[2px] px-[10px] mb-3 font-medium text-white text-[14px] rounded-full'>-10%</span>
+              </div>
+              <div className='border-dashed rounded-2xl border border-[#d2d2d2] py-3 px-2'>
+                <span className='text-sm text-center mb-2 block'>Sản phẩm bao gồm</span>
+                <div className='flex items-center justify-center flex-wrap gap-1'>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Bàn</span>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Ghế công thái học</span>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Arm</span>
+                  <span className='bg-[#e7e6f5] flex-shrink-0 text-black text-sm py-[3px] px-[10px] rounded-full'>Kê chân</span>
+                </div>
+              </div>
+              <Link href='/categories/ghe-cong-thai-hoc' className='border-[1.5px] w-fit mx-auto hover:bg-[#10069f] hover:text-white border-[#10069f] rounded-full py-2 px-4 mt-4 block text-[#10069f] text-center font-medium'>
+                <div className='group-hover:underline'>Chi tiết COMBO</div>
+              </Link>
+            </div>
+          </Link>
+        </div>
+        <Link href='/categories/ghe-cong-thai-hoc' className='border-[1.5px] w-fit mx-auto bg-[#10069f] border-white hover:border-[#10069f] rounded-xl py-[10px] px-[30px] flex justify-center items-center gap-1 text-[#fff] text-center font-medium'>
+          <div className='group-hover:underline'>Xem tất cả</div>
+          <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="m18 12l.354-.354l.353.354l-.353.354zm-12 .5a.5.5 0 0 1 0-1zm8.354-4.854l4 4l-.708.708l-4-4zm4 4.708l-4 4l-.708-.708l4-4zM18 12.5H6v-1h12z"></path></svg>
+        </Link>
+      </div>
+      <div className='w-52 h-52 bg-gray-200'></div>
+      <div className='w-52 h-52 bg-gray-200'></div>
+      <div className='w-52 h-52 bg-gray-200'></div>
     </>
   );
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,14 +4,16 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <div className='bg-[#282828] h-[32px] flex items-center justify-center text-center'>
+        <>
+            <div className='bg-[#282828] z-50 h-[32px] flex items-center justify-center text-center'>
                 <Link href={'/contact'}>
                     <span className='text-white text-[14px] hover:underline'>Miễn phí vận chuyển và lắp đặt tại HN và HCM</span>
                 </Link>
             </div>
-            <div className='max-w-screen-2xl mx-auto flex items-center justify-between p-4'>
-                <Image width={100} height={100} src='/logo.webp' alt='logo' />
+            <div className='max-w-screen-2xl sticky top-0 z-50 bg-white mx-auto flex items-center justify-between p-4'>
+                <Link href={'/'}>
+                    <Image width={100} height={100} src='/logo.webp' alt='logo' />
+                </Link>
                 <div className='flex items-center gap-3'>
                     <Link href={'/'}>
                         <span className='text-[16px] flex justify-center items-center text-[#000000bf] font-bold py-[8px] px-[20px] rounded-xl transition-all hover:text-[#10069f] hover:bg-[#10069f24]'>Ghế công thái học</span>
@@ -22,11 +24,28 @@ const Header = () => {
                     <Link href={'/contact'}>
                         <span className='text-[16px] flex justify-center items-center text-[#000000bf] font-bold py-[8px] px-[20px] rounded-xl transition-all hover:text-[#10069f] hover:bg-[#10069f24]'>Phụ kiện</span>
                     </Link>
+                    <Link href={'/contact'}>
+                        <span className='text-[16px] flex justify-center items-center text-[#000000bf] font-bold py-[8px] px-[20px] rounded-xl transition-all hover:text-[#10069f] hover:bg-[#10069f24]'>Góc chia sẻ</span>
+                    </Link>
+                    <Link href={'/contact'}>
+                        <span className='text-[16px] flex justify-center items-center text-[#000000bf] font-bold py-[8px] px-[20px] rounded-xl transition-all hover:text-[#10069f] hover:bg-[#10069f24]'>Hỗ trợ Khách hàng</span>
+                    </Link>
                     
                 </div>
+                <div className='flex items-center gap-3'>
+                    <div className='cursor-pointer'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} className='hover:scale-105' viewBox="0 0 24 24"><g fill="none" stroke="currentColor"><circle cx={11} cy={11} r={6}></circle><path strokeLinecap="round" d="m20 20l-3-3"></path></g></svg>
+                    </div>
+                    <Link href={'/account'}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} className='hover:scale-105' viewBox="0 0 24 24"><g fill="none" stroke="currentColor" strokeLinecap="round"><circle cx={12} cy={8} r={3.5}></circle><path d="M4.85 16.948c.639-2.345 3.065-3.448 5.495-3.448h3.31c2.43 0 4.856 1.103 5.496 3.448a10 10 0 0 1 .295 1.553c.06.55-.394.999-.946.999h-13c-.552 0-1.005-.45-.946-.998a10 10 0 0 1 .295-1.554Z"></path></g></svg>
+                    </Link>
+                    <div className='cursor-pointer relative'>
+                        <span className='w-4 h-4 flex justify-center items-center text-[10px] font-medium absolute -right-1 top-3 rounded-full bg-[#10069f] text-white'>0</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} className='hover:scale-105' viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" d="m10.5 15.5l-1-3.5m-1-7.5l-2 3m9-3l2 3m-4 8l1-3.5m-10-2.5v0c.584 0 1.09.406 1.217.976l1.087 4.892c.334 1.501.5 2.252 1.049 2.692s1.317.44 2.856.44h2.582c1.539 0 2.308 0 2.856-.44c.549-.44.715-1.19 1.05-2.692l1.086-4.892c.127-.57.633-.976 1.217-.976v0m0 0h-15a1 1 0 0 1 0-2h15a1 1 0 1 1 0 2Z"></path></svg>
+                    </div>
+                </div>
             </div>
-            
-        </div>
+        </>
     );
 };
 
