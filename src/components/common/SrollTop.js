c3f9f090dc0234684a1ca67afc4fd140f0143d9a
Phạm Văn Hoàng <iloveanh18@gmail.com>
2025-02-06 13:45:14 +0700

    update

---
+++ src/components/common/SrollTop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SrollTop = () => {
+    return (
+        <div className='z-50 fixed bottom-4 right-4'>
+            svg
+        </div>
+    );
+};
+
+export default SrollTop;
